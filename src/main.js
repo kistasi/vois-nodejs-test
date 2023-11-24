14628dd693e6b276001243c20285d5b451c8f27e
Marton Tasnadi <marton.tasnadi@gmail.com>
2023-11-24 20:01:59 +0100

    Add first test endpoint

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
