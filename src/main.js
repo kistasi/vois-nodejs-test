b7c1ab969b4c44d041486383b8a2894f5039f252
Marton Tasnadi <marton.tasnadi@gmail.com>
2023-11-25 15:00:49 +0100

    Add helmet package

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const helmet = require('helmet');
 const apiRouter = require('./routers/api');
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(helmet());
 
 app.use(express.static(path.join(__dirname, 'frontend')));
 
